4b363b6f281558a78572fc3e160c04faca1899fe
nickyui99 <93330502+nickyui99@users.noreply.github.com>
2023-03-28 21:10:48 +0800

    update progress

--- src/app/pages/home/home.page.spec.ts
+++ src/app/pages/home/home.page.spec.ts
@@ -2,10 +2,12 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
+import {Router} from "@angular/router";
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -14,11 +16,24 @@ describe('HomePage', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
+    router = TestBed.get(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should navigate to pickupcalls page when on click see all', () => {
+    spyOn(router, 'navigate');
+
+    component.seeAll();
+
+    expect(router.navigate).toHaveBeenCalledWith(['pickupcalls']);
+  });
+
+  it('should navigate to pickupcalls page when on click see all', () => {
+    spyOn(router, 'navigate');
+
+    component.createPickupCall();
+
+    expect(router.navigate).toHaveBeenCalledWith(['pickupcall']);
   });
 });
--- src/app/pages/pickupcall/pickupcall.page.spec.ts
+++ src/app/pages/pickupcall/pickupcall.page.spec.ts
@@ -2,10 +2,12 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { PickupcallPage } from './pickupcall.page';
+import {Router} from "@angular/router";
 
 describe('PickupcallPage', () => {
   let component: PickupcallPage;
   let fixture: ComponentFixture<PickupcallPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -14,11 +16,16 @@ describe('PickupcallPage', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(PickupcallPage);
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should navigate the user to the home screen when on create pickup call clicked', () => {
+    spyOn(router, 'navigate');
+
+    component.onCreatePickupCall();
+
+    expect(router.navigate).toHaveBeenCalledWith(['home']);
   });
 });
