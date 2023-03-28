a3275b9b748929b650606cc3b3ee6d330f988f1c
Nicholas Ooi Zhee Chen <93330502+nickyui99@users.noreply.github.com>
2023-03-28 10:49:54 +0800

    update progress

--- src/app/pages/loader/loader.module.ts
+++ src/app/pages/loader/loader.module.ts
@@ -4,17 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-import { FolderPageRoutingModule } from './folder-routing.module';
+import { LoaderPageRoutingModule } from './loader-routing.module';
 
-import { FolderPage } from './folder.page';
+import { LoaderPage } from './loader.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    LoaderPageRoutingModule,
   ],
-  declarations: [FolderPage]
+  declarations: [LoaderPage]
 })
-export class FolderPageModule {}
+export class LoaderPageModule {}
---
+++ src/app/pages/loader/loader.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.page.html',
+  styleUrls: ['./loader.page.scss'],
+})
+export class LoaderPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log("Hello World");
+  }
+
+}
